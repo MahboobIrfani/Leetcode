8ce13d8edc5664dc13d81e889199fb648988afcb
MahboobIrfani <139584868+MahboobIrfani@users.noreply.github.com>
2024-07-31 16:56:36 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (69.12%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,42 +1,41 @@
 class Solution {
-    static void rotate(int[][] matrix){
-        int n=matrix.length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;
-            }
-        }
-        int start=0;
-        int end=matrix.length-1;
-        while(start<=end){
-            for(int row=0;row<matrix.length;row++){
-                int temp=matrix[row][start];
-                matrix[row][start]=matrix[row][end];
-                matrix[row][end]=temp;
-            }
-                start++;
-                end--;
-        }
+    public boolean findRotation(int[][] mat, int[][] target) {
+      for(int i=0; i<4; i++){
+          if(check(mat,target)){
+              return true;
+          }
+          rotate(mat);
+      }
+        return false;
     }
-    static boolean check(int[][] matrix,int[][] target){
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix.length;j++){
-                if(matrix[i][j]!=target[i][j]){
-                return false;
+    public static boolean check(int[][] mat, int[][] target){
+        for(int i=0; i<mat.length;i++){
+            for(int j=0; j<mat.length;j++){
+                if(mat[i][j]!=target[i][j]){
+                    return false;
                 }
             }
         }
         return true;
     }
-    public boolean findRotation(int[][] mat, int[][] target) {
-      for(int i=0;i<4;i++){
-       if(check(mat,target)){
-           return true;
-       } 
-      rotate(mat);
-      }
-      return false;
+    public static void rotate(int[][] mat){
+        for(int i=0; i<mat.length;i++){
+            for(int j=0;j<i;j++){
+                int temp=mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i]=temp;
+            }
+        }
+        int start=0;
+        int end=mat.length-1;
+        while(start<=end){
+            for(int row=0;row<mat.length;row++){
+                int temp=mat[row][start];
+                mat[row][start]=mat[row][end];
+                mat[row][end]=temp;
+            }
+            start++;
+            end--;
+        }
     }
 }
