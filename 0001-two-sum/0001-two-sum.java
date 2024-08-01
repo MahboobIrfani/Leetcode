7a85a658918a11df94df40e27a0aea43ab7371fb
MahboobIrfani <139584868+MahboobIrfani@users.noreply.github.com>
2024-08-01 12:28:53 +0530

    Update 0001-two-sum.java
    time complexity less than O(n^2).

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-      int n= nums.length;
+     /* int n= nums.length;
          int[] ans= new int[2];
         for(int i=0; i<n;i++){
             for(int j=0;j<n; j++){
@@ -10,6 +10,17 @@ class Solution {
                 }
             }
         }
-        return ans;
+        return ans;*/
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0; i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        for(int i=0; i<nums.length;i++){
+            int complement= target-nums[i];
+            if(map.containsKey(complement) && map.get(complement)!=i){
+                return new int[]{i,map.get(complement)};
+        }
     }
+        return new int[]{-1,-1};
+ }
 }
