710213fa09c320146c9e9731682ca12ab937af7c
MahboobIrfani <139584868+MahboobIrfani@users.noreply.github.com>
2024-08-05 16:09:16 +0530

    Time: 1 ms (100.00%), Space: 57.2 MB (55.19%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -21,24 +21,23 @@ class Solution {
         return n+1;
     }*/
     int i=0;
-    while(i<nums.length){
-        int correct=nums[i]-1;
-        if(nums[i]>0 && nums[i]<=nums.length && nums[i]!=nums[correct]){
-            swap(nums,i,correct);
-        }else{
-            i++;
+        while(i<nums.length){
+            int correct=nums[i]-1;
+            if(nums[i]>0&& nums[i]<nums.length && nums[i]!=nums[correct]){
+                swap(nums,i, correct);
+            }else{
+                i++;
+            }
         }
-    }
-    for(int index=0; index<nums.length; index++){
-        if(nums[index]!=index+1){
+        for(int index=0;index<nums.length;index++){
+            if(nums[index]!=index+1)
             return index+1;
         }
+        return nums.length+1;
     }
-    return nums.length+1;
-    }
-    public static void swap(int[] nums, int first, int second){
-        int temp=nums[first];
-        nums[first]=nums[second];
-        nums[second]=temp;
+    public static void swap(int[] nums, int a, int b){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
     }
 }
